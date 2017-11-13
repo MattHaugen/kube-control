4acb03750b5ac8b6bcf9f86082e911315d1a564f
Matt Haugen <matthew.haugen@target.com>
2017-11-13 16:35:57 -0600

    Replica set describe

--- src/app/components/replicaset-manager/replicaset-manager.component.ts
+++ src/app/components/replicaset-manager/replicaset-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { KubectlService } from '../../providers/kubectl.service';
 import { Subject } from 'rxjs/Subject';
+import { SuiModalService } from 'ng2-semantic-ui';
+import { TerminalOutputModal } from '../terminal-output-modal/terminal-output-modal.component';
 
 @Component({
   selector: 'replicaset-manager',
@@ -10,11 +12,12 @@ export class ReplicasetManagerComponent implements OnInit {
   @Input() context: string;
   @Input() refreshListener:Subject<any>;
   data: Array<object> = [];
-  headerLabels = ['Name', 'Desired', 'Current', 'Ready', 'Age'];
+  headerLabels = ['Name', 'Desired', 'Current', 'Ready', 'Age', ''];
   loading: boolean = true;
 
   constructor(
     private kubectlService: KubectlService,
+    private modalService: SuiModalService
   ) {}
 
   ngOnInit() {
@@ -33,6 +36,13 @@ export class ReplicasetManagerComponent implements OnInit {
     this.kubectlService.getResource(this.context, 'replicasets').then(resourceDetails => {
       this.data = resourceDetails;
       this.loading = false;
+    });
+  }
+
+  showDescribe(replicasetName: string): void {
+    this.kubectlService.getDescribe('rs/' + replicasetName).then(logs => {
+      this.modalService
+      .open(new TerminalOutputModal(logs, replicasetName));
     });
   }
 }
