5a6cd940e4e0bd3ed0ffa281c414bd17559b4b46
Matt Haugen <matthew.haugen@target.com>
2017-11-13 16:27:37 -0600

    Service describe

--- src/app/components/service-manager/service-manager.component.ts
+++ src/app/components/service-manager/service-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { KubectlService } from '../../providers/kubectl.service';
 import { Subject } from 'rxjs/Subject';
+import { SuiModalService } from 'ng2-semantic-ui';
+import { TerminalOutputModal } from '../terminal-output-modal/terminal-output-modal.component';
 
 @Component({
   selector: 'service-manager',
@@ -10,11 +12,12 @@ export class ServiceManagerComponent implements OnInit {
   @Input() context: string;
   @Input() refreshListener:Subject<any>;
   data: Array<object> = [];
-  headerLabels = ['Name', 'Cluster-IP', 'External-IP', 'Port(s)', 'Age'];
+  headerLabels = ['Name', 'Cluster-IP', 'External-IP', 'Port(s)', 'Age', ''];
   loading: boolean = true;
 
   constructor(
     private kubectlService: KubectlService,
+    private modalService: SuiModalService
   ) {}
 
   ngOnInit() {
@@ -33,6 +36,13 @@ export class ServiceManagerComponent implements OnInit {
     this.kubectlService.getResource(this.context, 'services').then(resourceDetails => {
       this.data = resourceDetails;
       this.loading = false;
+    });
+  }
+
+  showDescribe(serviceName: string): void {
+    this.kubectlService.getDescribe('svc/' + serviceName).then(logs => {
+      this.modalService
+      .open(new TerminalOutputModal(logs, serviceName));
     });
   }
 }
