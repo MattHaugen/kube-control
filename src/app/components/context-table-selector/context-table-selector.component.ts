bb6eeaec0be2a818de6d59aeae9e3090235aa1c7
Matt Haugen <matthew.haugen@target.com>
2017-12-23 22:39:23 -0600

    Filter out already selected contexts

--- src/app/components/context-table-selector/context-table-selector.component.ts
+++ src/app/components/context-table-selector/context-table-selector.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { KubectlService } from '../../providers/kubectl.service';
 
 @Component({
   selector: 'context-table-selector',
-  templateUrl: './context-table-selector.component.html'
+  templateUrl: './context-table-selector.component.html',
+  styleUrls: ['./context-table-selector.component.scss']
 })
 export class ContextTableSelectorComponent implements OnInit {
   @Output() onSelection = new EventEmitter<string>();
+  @Input() alreadySelected: { context: string }[];
   availableContexts: Array<object> = [];
   filteredContexts: Array<object> = [];
   filterInput: string = null;
@@ -18,6 +20,12 @@ export class ContextTableSelectorComponent implements OnInit {
 
   ngOnInit() {
     this.kubectlService.getContexts().then(contextDetails => {
+      // Filter out any contexts that already have a tab
+      const activeContexts = this.alreadySelected.map(obj => { return obj.context; });
+      contextDetails = contextDetails.filter(
+        (availableContext: { name: string }) => { return !activeContexts.includes(availableContext.name )}
+      )
+
       this.availableContexts = Object.assign([], contextDetails);
       this.filteredContexts = Object.assign([], contextDetails);
     });
@@ -29,7 +37,7 @@ export class ContextTableSelectorComponent implements OnInit {
 
   onFilterInput(inputText: string) {
     if (inputText) {
-      this.filteredContexts = this.availableContexts.filter((contextDetails: { name:string }) => {
+      this.filteredContexts = this.availableContexts.filter((contextDetails: { name: string }) => {
         return contextDetails.name.includes(inputText);
       });
     } else {
