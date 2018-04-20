eca615aafec8f324c9ce81e7c86727f119b1aa14
Matt Haugen <mhaugen@matthaugen.com>
2018-04-19 22:42:51 -0500

    Start integrating React

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Home } from './home';
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
---
+++ src/home.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export class Home extends React.Component {
+
+	render() {
+		return (<h1>Hello</h1>);
+	}
+}
