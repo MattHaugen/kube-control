72fc1fb13f8a2ffc8c80c92082bea668d19318b3
Matt Haugen <mhaugen@matthaugen.com>
2018-04-21 15:44:18 -0500

    Hook up Redux and DevTools

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow } from 'electron';
 import { enableLiveReload } from 'electron-compile';
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer';
 
 enableLiveReload({strategy: 'react-hmr'});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+app.quit();
 }
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -13,25 +14,33 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
-  });
+   //Set up DevTools
+   const extensions = [
+   installExtension(REACT_DEVELOPER_TOOLS),
+   installExtension(REDUX_DEVTOOLS)
+];
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+Promise.all(extensions).then(() => {
+   // Create the browser window.
+   mainWindow = new BrowserWindow({
+   width: 1024,
+   height: 768,
+})
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+// and load the index.html of the app.
+mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+// Open the DevTools.
+mainWindow.webContents.openDevTools();
+
+// Emitted when the window is closed.
+mainWindow.on('closed', () => {
+// Dereference the window object, usually you would store windows
+// in an array if your app supports multi windows, this is the time
+// when you should delete the corresponding element.
+mainWindow = null;
+});
+});
 };
 
 // This method will be called when Electron has finished
@@ -41,19 +50,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+// On OS X it is common for applications and their menu bar
+// to stay active until the user quits explicitly with Cmd + Q
+if (process.platform !== 'darwin') {
+app.quit();
+}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+   // On OS X it's common to re-create a window in the app when the
+   // dock icon is clicked and there are no other windows open.
+   if (mainWindow === null) {
+   createWindow();
+}
 });
 
 // In this file you can include the rest of your app's specific main process
---
+++ src/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import initialize from './initialize';
+
+const rootReducer = combineReducers({
+	initialize
+});
+
+export default rootReducer;
