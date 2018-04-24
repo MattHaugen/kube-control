b71d4eb6eb8c33f629a224f6e5cb49c35c67328a
Matt Haugen <mhaugen@matthaugen.com>
2018-04-23 22:26:19 -0500

    React Router

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
 import initialize from "./initialize";
 
 const rootReducer = combineReducers({
-   initialize
+   initialize,
+   router: routerReducer
 });
 
 export default rootReducer;
